4a17fdd9ede591309d34d937d330ce9d6d2e666d
Torstein A. Bø <361267+tibnor@users.noreply.github.com>
2023-10-29 13:27:30 +0000

    startet med progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,27 +5,63 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const Element = styled.progress`
-
-  /* Reset the default appearance */
-  -webkit-appearance: none;
-   appearance: none;
-
-  width: 250px;
-  height: 20px;
-  &::-webkit-progress-bar {
-    background-color: ${COLORS.transparentGray15};
-    border-radius: 4px;
-  }
-
-  &::-webkit-progress-value {
-      height: 12px
-      background-color: ${COLORS.primary}; 
-  }
+const Wrapper = styled.div`
+  display: block;
+  width: 370px;
+  height: ${p => p.theme.barHeight}px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: ${p => p.theme.wrapperPadding};
+  border-radius: 4px;
+  position: relative;
+  background-color: ${COLORS.gray50};
 `
 
+const Bar = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  background-color: ${COLORS.primary};
+`
+
+const Hider = styled.div`
+  display: block;
+  position: absolute;
+  width: ${p => p.width}%;
+  height: 100%;
+  right: 0;
+  top: 0;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 0 4px 4px 0;
+  background-color: ${COLORS.gray50};
+`
+
+
+
 const ProgressBar = ({ value, size }) => {
-  return <Element value={value/100} />;
+  let theme;
+  if (size === "small") {
+    theme = {
+      
+      barHeight: 8,
+      barRadius: 4,
+    }
+  } else if (size === "medium") {
+    theme = {
+      barHeight: 12,
+      barRadius: 4,
+    }
+  } else if (size === "large") {
+    theme = {
+      wrapperPadding: "4px",
+      barHeight: 24,
+      barRadius: 4,
+    }
+  }
+
+  return <Wrapper theme={theme} role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow={value}>
+    <Bar theme={theme}></Bar>
+    <Hider width={(100-value)}></Hider>
+  </Wrapper>;
 };
 
 export default ProgressBar;
