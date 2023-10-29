9a6ced903ab20662ee61103e42b79e75d19c2b00
Torstein I. Bø <torsteinibo@gmail.com>
2023-10-29 12:19:27 +0000

    initial

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,27 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Element = styled.progress`
+
+  /* Reset the default appearance */
+  -webkit-appearance: none;
+   appearance: none;
+
+  width: 250px;
+  height: 20px;
+  &::-webkit-progress-bar {
+    background-color: ${COLORS.transparentGray15};
+    border-radius: 4px;
+  }
+
+  &::-webkit-progress-value {
+      height: 12px
+      background-color: ${COLORS.primary}; 
+  }
+`
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <Element value={value/100} />;
 };
 
 export default ProgressBar;
